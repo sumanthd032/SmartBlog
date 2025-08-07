c91002635b55eb521ba9e561171dd11aa05cde88
sumanthd032 <sumanthd032@gmail.com>
2025-08-07 23:41:51 +0530

    Create database models and from that database file

---
+++ models.py
@@ -0,0 +1,25 @@
+import datetime
+from sqlalchemy import DateTime, ForeignKey, Column, String, Integer, Text
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+    posts = relationship("Post", back_populates="author")
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    content = Column(Text)
+    author_id = Column(Integer, ForeignKey("users.id"))
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+    author = relationship("User", back_populates="posts")
