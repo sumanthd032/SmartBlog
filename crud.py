4f6f9c774a41e53a165858d3f1e1b3af321e942d
sumanthd032 <sumanthd032@gmail.com>
2025-08-09 22:28:36 +0530

    create new helper function for auth and corrected user creation function

--- crud.py
+++ crud.py
@@ -1,14 +1,20 @@
 from sqlalchemy.orm import Session
-from passlib.context import CryptContext
-import models, schemas
+import models, schemas, auth
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+def authenticate_user(db: Session, username: str, password: str):
+    """Finds a user and verifies their password."""
+    user = get_user_by_username(db, username=username)
+    if not user:
+        return False
+    if not auth.verify_password(password, user.hashed_password):
+        return False
+    return user
 
 def get_user_by_username(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
 
 def create_user(db: Session, user: schemas.UserCreate):
-    hashed_password = pwd_context.hash(user.password)
+    hashed_password = auth.pwd_context.hash(user.password)
     db_user = models.User(username=user.username, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
@@ -24,3 +30,12 @@ def create_user_post(db: Session, post: schemas.PostCreate, user_id: int):
     db.commit()
     db.refresh(db_post)
     return db_post
+
+def authenticate_user(db: Session, username: str, password: str):
+    """Finds a user and verifies their password."""
+    user = get_user_by_username(db, username=username)
+    if not user:
+        return False
+    if not auth.verify_password(password, user.hashed_password):
+        return False
+    return user
