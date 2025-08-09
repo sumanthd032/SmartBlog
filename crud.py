eef4cc84407f3a10575ffc2a70df4a912d5a2edf
sumanthd032 <sumanthd032@gmail.com>
2025-08-09 22:11:03 +0530

    update crud with user functions to handle user creation

--- crud.py
+++ crud.py
@@ -1,5 +1,19 @@
 from sqlalchemy.orm import Session
+from passlib.context import CryptContext
 import models, schemas
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = pwd_context.hash(user.password)
+    db_user = models.User(username=user.username, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
 
 def get_posts(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Post).offset(skip).limit(limit).all()
